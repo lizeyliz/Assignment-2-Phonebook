ed22e9f28c62cf02768d8b273542ad98d3642266
lizeyliz <alizaeaton@gmail.com>
2024-10-24 17:07:10 -0700

    stopping here bc I'm tired and messing things up

--- Contact.java
+++ Contact.java
@@ -2,26 +2,67 @@
 //object class
 //a single list note representing one contact in phonebook
 public class Contact {
-    //instance variables (make private and add getters and setters later)
-    public String firstName;
-    public String lastName;
-    public String phoneNum;
-    public String address;
-    public String city; //not sure if I want this field
+    //instance variables
+    private String name;
+    private String phoneNum;
+    private String address;
+    private String city; //not sure if I want this field
+
+    private String[] contact;
 
     //constructor
     public Contact (String firstName, String lastName, String phoneNum, String address, String city) {
-        this.firstName = firstName;
+        /*this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNum = phoneNum;
         this.address = address;
-        this.city = city;
+        this.city = city;*/
+        contact = new String[4];
     } //end constructor
+
+    public void setContact(String[] newContact) {
+        this.contact = newContact;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+     public String getName() {
+        return name;
+     } //end getName
+
+     public void setName(String newName) {
+        this.name = newName;
+     }//end setName
+
+     public String getPhoneNum() {
+        return phoneNum;
+     } //end getPhoneNum
+
+     public void setPhoneNum(String newPhoneNum) {
+        this.phoneNum = newPhoneNum;
+     }//end setPhoneNum
+
+     public String getAddress() {
+        return address;
+     } //end getAddress
+
+     public void setAddress(String newAddress) {
+        this.address = newAddress;
+     }//end setAddress
+
+     public String getCity() {
+        return city;
+     }//end getCity
+
+     public void setCity(String newCity) {
+        this.city = newCity;
+     }//end setCity
  
     //put contact into presentable form
     public String toString() {
-        String contactString = firstName + " " + lastName + "\n"
-        + phoneNum + "\n" + address + "\n" + city + "\n";
+        String contactString = name + "\n" + phoneNum + "\n" + address + "\n" + city + "\n";
         return contactString;
     }//end toString
 }
--- aePhonebookTest.java
+++ aePhonebookTest.java
@@ -2,16 +2,14 @@ import java.util.Scanner;
 public class aePhonebookTest {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Contact p1 = new Contact(null, null, null, null, null);
-        Contact p2 = new Contact("Harry", "Potter", "206-676-8831", 
-        "#12 Grimmauld Place", "London");
+        Contact p1 = new Contact();
+        Contact p2 = new Contact();
         System.out.println(p2.toString());
 
         //Create contact from user input (for p1)
-        System.out.println("Enter first name:");
-        p1.firstName = scan.next();
-        System.out.println("Enter last name");
-        p1.lastName = scan.next();
+        System.out.println("Enter name:");
+        p1.name = scan.nextLine();
+        scan.nextLine();//cosumes dangling newline char
         System.out.println("Enter phone number (XXX-XXX-XXXX):");
         p1.phoneNum = scan.next();
         scan.nextLine();//cosumes dangling newline char
