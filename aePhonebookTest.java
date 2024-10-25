b298d7d1ba4d653d2bc6ea80e8d2f97eaa8762b8
lizeyliz <alizaeaton@gmail.com>
2024-10-24 17:25:34 -0700

    created createContact method (currently in test class)

--- aePhonebookTest.java
+++ aePhonebookTest.java
@@ -2,27 +2,35 @@ import java.util.Scanner;
 public class aePhonebookTest {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Contact p1 = new Contact("Harry Potter", "206-682-7855", "12 Grimmauld Place", "London");
-        Contact p2 = new Contact("John Doe", "206-574-6779", "1479 Harris Ave", "Bellingham");
+        //test
+        Contact p1 = new Contact("Harry Potter", "206-682-7855", "12 Grimmauld Place",
+        "London");
+        Contact p2 = new Contact("John Doe", "206-574-6779", "1479 Harris Ave", 
+        "Bellingham");
+        System.out.println(p1.toString());
         System.out.println(p2.toString());
 
-        //Create contact from user input (for p1)
+        Contact p3 = new Contact(null, null, null, null);
+
+        createContact(scan, p3);
+        System.out.println();
+        System.out.println(p3.toString());
+    }//end main
+
+    //Create contact from user input
+    public static Contact createContact(Scanner scan, Contact myContact) {
         System.out.println("Enter name:");
-        p1.name = scan.nextLine();
-        scan.nextLine();//cosumes dangling newline char
+        myContact.name = scan.nextLine();
         System.out.println("Enter phone number (XXX-XXX-XXXX):");
-        p1.phoneNum = scan.next();
+        myContact.phoneNum = scan.next();
         scan.nextLine();//cosumes dangling newline char
         System.out.println("Enter address:");
-        p1.address = scan.nextLine();
+        myContact.address = scan.nextLine();
         System.out.println("Enter city:");
-        p1.city = scan.next();
+        myContact.city = scan.next();
 
-        //print p1
-        System.out.println();
-        System.out.println(p1.toString());
-
-    }//end main
+        return myContact;
+    }
 
     //user menu
     public void userMenu(){
